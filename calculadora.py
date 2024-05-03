2c99c9a5029ec0995917cee72c50cbd1213dcd80
Amanda Cerimac <168143086+acerimac@users.noreply.github.com>
2024-05-03 19:14:15 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+n1 = float(input('>>'))
+n2 = float(input('>>'))
+soma = n1+n2
+print ('resultado da soma: ', soma ) 
+
+subtracao = n1-n2
+print('resultado da subtraçao: ', subtracao) 
+
+multiplicacao = n1*n2
+print('resultado da multiplicacao: ', multiplicacao) 
+
+divisao = n1/n2
+print('resultado da divisao: ', divisao) 
